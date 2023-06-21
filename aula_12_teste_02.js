f50c10f803152a20643f1fe0c163142a9bd74508
Gabriel Mautone Costa <128853409+Mautone15@users.noreply.github.com>
2023-06-21 15:57:36 -0300

    aula_12_teste_02.js
    TESTE DE NUMEROS IMPARES!

---
+++ aula_12_teste_02.js
@@ -0,0 +1,21 @@
+// test //
+
+const QUANTIDADE = 6;
+
+
+function numerosImpares(valor) {
+    let contar = 0;
+    let novoValor = valor;
+    
+    
+    while(contar < QUANTIDADE) {
+        if (novoValor % 2 !== 0) {
+            console.log(novoValor);
+            contar++;
+        }
+
+        novoValor++;
+    }
+}
+
+numerosImpares(8);
