9ace9f6471edc30e7e1345219647588f85c81e57
Gabriel Mautone Costa <128853409+Mautone15@users.noreply.github.com>
2023-06-21 14:10:08 -0300

    aula_08.js
    A conversão de String para Numero e Numero para String.

---
+++ aula_08.js
@@ -0,0 +1,8 @@
+// conversão de String para Numero e Numero para String //
+
+const salario = '103';
+const salarioNumero = Number(salario);
+
+console.log('salario: ', salario);
+console.log('salario numero:', salarioNumero);
+console.log('salario: ', Number(salario).toFixed(2));
