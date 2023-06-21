d72a03b7fa132dc028ee19133d0bdba29a01a744
Gabriel Mautone Costa <128853409+Mautone15@users.noreply.github.com>
2023-06-21 14:18:33 -0300

    aula_09.js
    Criando e executando uma função.

---
+++ aula_09.js
@@ -0,0 +1,13 @@
+// criando e executando uma função //
+
+function soma(item1, item2) {
+    return item1 + item2;
+}
+
+function diminuir(item1, item2) {
+    return item1 - item2;
+}
+
+console.log('SOMA: ', soma(5, 6));
+console.log('SOMA: ', soma(643, 64373));
+console.log('SOMA: ', diminuir(643, 65373));
