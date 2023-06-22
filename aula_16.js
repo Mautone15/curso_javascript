fdb2478d533ac5f3ffd0a28c35a487d88fe3d383
Gabriel Mautone Costa <128853409+Mautone15@users.noreply.github.com>
2023-06-22 14:32:51 -0300

    aula_16.js
    Variáveis primitivas e imutáveis.

---
+++ aula_16.js
@@ -0,0 +1,14 @@
+// Variaveis Primitivas e imutaveis //
+
+const numero = 4324;
+const stringA ='fjfdghd';
+const verdadeiroOuFaslo = true; // false
+
+const arrayTest = [432, 6436, 643];
+
+stringA[4] = 'JULIO';
+
+arrayTest.push(473985);
+
+console.log(arrayTest);
+console.log(stringA);
