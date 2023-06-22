7145990ee1be40b00e1da7d46c01b97e45dbf1f0
Gabriel Mautone Costa <128853409+Mautone15@users.noreply.github.com>
2023-06-22 16:20:42 -0300

    aula_23.js
    Função recursiva.

---
+++ aula_23.js
@@ -0,0 +1,10 @@
+// FUNÇAO RECURSIVA
+
+const fatorial = (valor) => {
+    if (valor === 0 || valor === 1) {
+        return 1;
+    }
+    return valor * fatorial(valor - 1);
+}
+
+console.log(fatorial(4));
