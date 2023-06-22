e7593d0e15c4709e4e704047bd456a3eeff87d89
Gabriel Mautone Costa <128853409+Mautone15@users.noreply.github.com>
2023-06-22 14:49:13 -0300

    aula_17.js
    Aprendendo sobre SWITCH CASE.

---
+++ aula_17.js
@@ -0,0 +1,20 @@
+// aprendendo sobre SWITCH CASE //
+
+function teste(key) {
+    switch (key) {
+        case 'Gabriel':
+            console.log('Gabriel Aqui')
+            break;
+        case 'Gustavo':
+            console.log('Gustavo Aqui')
+            break;
+        case 'Gerlaine':
+            console.log('Gerlaine Aqui')
+            break;
+        default:
+            console.log('Qualque Outro Aqui')
+            break;
+    }
+}
+
+teste('Gabriel')
